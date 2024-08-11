3158d9404c8ecdba102673787fdf1fa778e1f37d
Gusthawo Junkes <gusthawojunkes14@gmail.com>
2024-08-11 17:36:04 -0300

    feat: adding throws for transaction processor interface

--- src/main/kotlin/dev/wo/domain/services/TransactionProcessor.kt
+++ src/main/kotlin/dev/wo/domain/services/TransactionProcessor.kt
@@ -1,5 +1,6 @@
 package dev.wo.domain.services
 
+import dev.wo.domain.exceptions.FileProcessingException
 import dev.wo.domain.transactions.FinancialTransaction
 import dev.wo.domain.models.ofx.OFXFile
 import java.io.File
@@ -7,8 +8,13 @@ import java.io.File
 interface TransactionProcessor {
     var fileType: String?
     var file: File?
+
+    @Throws(FileProcessingException::class)
     fun processFile(): MutableList<FinancialTransaction>
+
     fun createFinancialTransactions(data: OFXFile): MutableList<FinancialTransaction>
+
     fun withFileType(fileType: String) = fileType.also { this.fileType = it }
+
     fun withFile(file: File?) = file.also { this.file = it }
 }
