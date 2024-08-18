8774a9a440bd7e37a40036b1360ad3850431bdb8
Gusthawo Junkes <gusthawojunkes14@gmail.com>
2024-08-18 14:47:38 -0300

    test: new processor options

--- src/test/kotlin/dev/wo/infrastructure/factories/TransactionProcessorFactoryTest.kt
+++ src/test/kotlin/dev/wo/infrastructure/factories/TransactionProcessorFactoryTest.kt
@@ -2,6 +2,7 @@ package dev.wo.infrastructure.factories
 
 import dev.wo.domain.enums.FinancialInstitution
 import dev.wo.domain.exceptions.FileProcessingException
+import dev.wo.infrastructure.adapters.processors.CommonCSVTransactionProcessor
 import dev.wo.infrastructure.adapters.processors.NubankOFXTransactionProcessor
 import org.junit.Assert.assertThrows
 import kotlin.test.Test
@@ -11,13 +12,13 @@ import kotlin.test.assertTrue
 class TransactionProcessorFactoryTest {
 
     @Test
-    fun `should return NubankOFXTransactionProcessor`() {
+    fun `should return NubankOFXTransactionProcessor when institution is Nubank and file type is OFX`() {
         val processor = TransactionProcessorFactory.getProcessor(FinancialInstitution.NUBANK, "OFX")
         assertTrue(processor is NubankOFXTransactionProcessor)
     }
 
     @Test
-    fun `should throw FileProcessingException for Nubank when file type is unkown`() {
+    fun `should throw FileProcessingException when institution is Nubank and file type is unkown`() {
 
         val exception = assertThrows(FileProcessingException::class.java) {
             TransactionProcessorFactory.getProcessor(FinancialInstitution.NUBANK, "xyz")
@@ -27,12 +28,27 @@ class TransactionProcessorFactoryTest {
     }
 
     @Test
-    fun `should throw FileProcessingException`() {
+    fun `should throw FileProcessingException when institution is Unkown`() {
         val exception = assertThrows(FileProcessingException::class.java) {
             TransactionProcessorFactory.getProcessor(FinancialInstitution.UNKNOWN, "unknown")
         }
 
         assertEquals("Invalid financial institution", exception.message)
+    }
+
+    @Test
+    fun `should return CommonCSVTransactionProcessor when institution is Any`() {
+        val processor = TransactionProcessorFactory.getProcessor(FinancialInstitution.ANY, "CSV")
+        assertTrue(processor is CommonCSVTransactionProcessor)
+    }
+
+    @Test
+    fun `should throw FileProcessingException when institution is Any and file type is unkown`() {
+        val exception = assertThrows(FileProcessingException::class.java) {
+            TransactionProcessorFactory.getProcessor(FinancialInstitution.ANY, "xyz")
+        }
+
+        assertEquals("Invalid file type for Any", exception.message)
     }
 
 }
