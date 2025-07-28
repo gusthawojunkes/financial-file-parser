fa3d7edd207387bab468025c06dc44dfe6d37704
Gusthawo Junkes <gusthawojunkes14@gmail.com>
2025-07-28 18:00:00 -0300

    feat: metrics scrapping configuration

--- src/main/kotlin/dev/wo/Application.kt
+++ src/main/kotlin/dev/wo/Application.kt
@@ -12,4 +12,5 @@ fun Application.module() {
     configureHTTP()
     configureRouting()
     configureCors()
+    configureMetrics()
 }
---
+++ src/main/kotlin/dev/wo/plugins/MicrometerMetrics.kt
@@ -0,0 +1,38 @@
+package dev.wo.plugins
+
+import io.ktor.server.application.Application
+import io.ktor.server.application.call
+import io.ktor.server.application.install
+import io.ktor.server.metrics.micrometer.MicrometerMetrics
+import io.ktor.server.response.respond
+import io.ktor.server.routing.get
+import io.ktor.server.routing.routing
+import io.micrometer.core.instrument.binder.jvm.JvmGcMetrics
+import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics
+import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics
+import io.micrometer.core.instrument.binder.system.FileDescriptorMetrics
+import io.micrometer.core.instrument.binder.system.ProcessorMetrics
+import io.micrometer.prometheusmetrics.PrometheusConfig
+import io.micrometer.prometheusmetrics.PrometheusMeterRegistry
+
+fun Application.configureMetrics(): PrometheusMeterRegistry {
+    val appMicrometerRegistry = PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
+
+    install(MicrometerMetrics) {
+        registry = appMicrometerRegistry
+        meterBinders = listOf(
+            JvmMemoryMetrics(),
+            JvmGcMetrics(),
+            ProcessorMetrics(),
+            JvmThreadMetrics(),
+            FileDescriptorMetrics()
+        )
+    }
+
+    routing {
+        get("/metrics") {
+            call.respond(appMicrometerRegistry.scrape())
+        }
+    }
+    return appMicrometerRegistry
+}
