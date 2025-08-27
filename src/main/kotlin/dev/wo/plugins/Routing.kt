fcad82619923566280e47fb1d6501c0714672b41
Gusthawo Junkes <gusthawojunkes14@gmail.com>
2025-08-27 14:39:06 -0300

    feat: demo page

--- src/main/kotlin/dev/wo/plugins/Routing.kt
+++ src/main/kotlin/dev/wo/plugins/Routing.kt
@@ -2,6 +2,7 @@ package dev.wo.plugins
 
 import dev.wo.application.web.routes.fileRouting
 import io.ktor.server.application.*
+import io.ktor.server.http.content.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
@@ -9,6 +10,14 @@ fun Application.configureRouting() {
     routing {
         get("/health-check") {
             call.respondText("Up and running!")
+        }
+
+        get("/demo") {
+            call.respondRedirect("/demo/index.html")
+        }
+
+        static("/demo") {
+            resources("static")
         }
 
         fileRouting()
