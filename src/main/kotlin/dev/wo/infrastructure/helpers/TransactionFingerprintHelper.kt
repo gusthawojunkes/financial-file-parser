99ee4be2ed705d64e896ee5fdb831732bb695438
Gusthawo Junkes <gusthawojunkes14@gmail.com>
2026-03-13 17:12:51 -0300

    fix: code generation

--- src/main/kotlin/dev/wo/infrastructure/helpers/TransactionFingerprintHelper.kt
+++ src/main/kotlin/dev/wo/infrastructure/helpers/TransactionFingerprintHelper.kt
@@ -49,7 +49,7 @@ object TransactionFingerprintHelper {
     private fun sha256(input: String): String {
         val digest = MessageDigest.getInstance("SHA-256")
         val hashBytes = digest.digest(input.toByteArray(Charsets.UTF_8))
-        return hashBytes.joinToString("") { "%02x".format(it) }
+        return hashBytes.joinToString("") { "%02x".format(it.toInt() and 0xFF) }
     }
 }
 
